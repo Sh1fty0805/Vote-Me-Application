5cd21ed6666ddb95a5b89d132d174fa93ec2d2c9
John Michael Calleja <johnmichael.calleja@benilde.edu.ph>
2025-06-18 07:23:45 +0800

    Initial commit

---
+++ VoteSmart/Home/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthdate = models.DateField()
+    address = models.CharField(max_length=255)
+    citizenship = models.CharField(max_length=50)
+    is_registered_voter = models.BooleanField(default=False)
+
+    #http://127.0.0.1:8000/Home or Senators or 
+
+    
+class Candidate(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    platform = models.TextField()
+    controversies = models.TextField(blank=True)
+    resume = models.TextField()
+    interview_links = models.URLField(blank=True)
+    area_coverage = models.CharField(max_length=255)
+
+    #image = models.ImageField(upload_to ='images/')
+
